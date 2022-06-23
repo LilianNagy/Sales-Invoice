7af0326557368fb79be1e4de5266107154f75f40
LilianNagy <Lilynagy2001@gmail.com>
2022-06-23 23:39:03 +0200

    View Pck incomplete
    In-Progress

---
+++ View/Frame.java
@@ -0,0 +1,23 @@
+package View;
+
+import java.awt.event.ActionListener;
+
+public class Frame extends javax.swing.JFrame {
+
+public Frame() {
+   
+}
+
+public ActionListener getListenerBtns() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+
+
+	  
+	     
+
+	
+}
